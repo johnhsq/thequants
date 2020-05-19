dfc286717d0dfeb8cf7964d08aa8520d5786916b
johnhsq <huang@huang-a04.vmware.com>
2020-05-19 06:33:36 -0700

    update

--- 04.portfolio/02.signals.to.positions.py
+++ 04.portfolio/02.signals.to.positions.py
@@ -3,7 +3,8 @@ import numpy as np
 prices = np.array([1, 3, -2, 9, 5, 7, 2])
 print(prices)
 
-# buy one share of stock when the price is above 2 dollars and the buy 3 more shares when it's above 4 dollars.
+# buy one share of stock when the price is above 2 dollars 
+# and the buy 3 more shares when it's above 4 dollars.
 signal_one = prices > 2
 signal_three = prices > 4
 
