b4cd7c1a337100a2f44de084d57e220e662fbc4d
John Huang <huang@huang-a03.vmware.com>
2021-04-20 10:52:53 -0700

    update

--- 04.portfolio/01.dtype.py
+++ 04.portfolio/01.dtype.py
@@ -5,12 +5,14 @@ array = np.arange(10)
 print(array)
 print(type(array))
 print(array.dtype)
+print('-'*50)
 
 float_arr = array / 2
 
 print(float_arr)
 print(type(float_arr))
 print(float_arr.dtype)
+print('-'*50)
 
 # cast float64 to int64
 int_arr = float_arr.astype(np.int64)
